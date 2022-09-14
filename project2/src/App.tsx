32e793aa7b95035e6cde004fc0fc0fc5c30da5d8
emilgau <emilgau@stud.ntnu.no>
2022-09-14 12:07:50 +0200

    Created getData function, can fetch data from git

--- project2/src/App.tsx
+++ project2/src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getData }from './api/fetch'
+import { Typography }from '@mui/material'
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData('17381', 'glpat-CRs4epaLyzKdvdpGzE_3')
+    .then((res) => {
+      console.log(res); // JSON data parsed by `data.json()` call
+      setData(res)
+    });
+  
+  } ,[]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
