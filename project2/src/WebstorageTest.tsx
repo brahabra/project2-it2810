f2548ef1c0d35e542a511040cc6b4e7bfb1cad95
tormadsen <tormads@ntnu.no>
2022-09-27 22:16:17 +0200

    Changelog: Made webstorage class simpler

--- project2/src/WebstorageTest.tsx
+++
@@ -1,6 +0,0 @@
-import { WebStorageTemp } from "./WebStorageTemp";
-
-let webStorage = new WebStorageTemp();
-let testArray = new Array();
-testArray.push(document.getElementById("test"));
-webStorage.initListeners(testArray);
