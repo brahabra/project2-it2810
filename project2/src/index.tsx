353095898445b0ec4befad4b5f0e2762e1c60429
emilgau <emilgau@stud.ntnu.no>
2022-09-29 15:36:40 +0200

    minor commit changes

--- project2/src/index.tsx
+++ project2/src/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { ProjectContextProvider } from './ProjectContext';
 import reportWebVitals from './reportWebVitals';
-import { ProjectContextProvider } from './ProjectContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
