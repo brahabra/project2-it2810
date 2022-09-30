9a1820bf96613347010b737122f89cd4b3479c1c
Mats Klevstad <matskl@stud.ntnu.no>
2022-09-30 11:56:29 +0200

    Add styles.tsx made by Tor to the new branch

---
+++ project2/src/styles/Styles.tsx
@@ -0,0 +1,68 @@
+import { height } from "@mui/system";
+
+export const style = {
+
+    graphContainer: {
+      height: '100%',
+      backgroundColor: "#FAFAFA"
+    },
+
+    chart: {
+      height: '50vh'
+    },
+
+    commitPaper: {
+      width: "50%",
+      height: '100%',
+      padding: '20px',
+      borderRadius: '25px',
+      margin: '20px',
+      backgroundColor: "#FAFAFA"
+    },
+
+    commitComponentPaper: {
+      padding: '20px',
+      borderRadius: '25px',
+      margin: '20px',
+      backgroundColor: "white"
+    },
+
+    issueListBox: {
+      width: "100%", 
+      margin: "auto", 
+      backgroundColor: "#DAF7A6"
+    },
+
+    chartNames: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+
+    removeTextMarg: {
+      padding: '0px',
+      paddingLeft: '4px',
+      margin: '0'
+    },
+
+    commitPage: {
+      width: "100%", 
+      margin: "auto"
+    },
+
+    commitsBox: {
+      padding: '20px',
+      borderRadius: '25px',
+      margin: 'auto',
+      width: "100%",
+      backgroundColor: "#FAFAFA"
+    },
+
+    flexRow: {
+      display: 'flex', 
+      flexDirection: 'row',
+      justifyContent: 'start',
+      marginBottom: '5px',
+    }
+
+}
