fe4388f101d79b11091a85c9857f584f1ee373da
Brage Hagemann Brataas <bragehb@stud.ntnu.no>
2022-10-03 13:42:57 +0200

    Installed whatwg-fetch
    Co-authored-by: Emil Gillingham Aukner <emilgau@stud.ntnu.no>

--- project2/src/tests/HomePage.test.tsx
+++ project2/src/tests/HomePage.test.tsx
@@ -3,22 +3,22 @@ import HomePage from '../pages/HomePage'
 import {render, cleanup, screen} from "@testing-library/react"
 import userEvent from '@testing-library/user-event';
 import { wait } from '@testing-library/user-event/dist/utils';
+import 'whatwg-fetch'
 
 const ReactTestRenderer = require('react-test-renderer');
 
-//afterEach(cleanup)
+afterEach(cleanup)
 
 it('renders correctly', () => {
   const tree = ReactTestRenderer.create(<HomePage />).toJSON()
   expect(tree).toMatchSnapshot()
 })
-/*
+
 it('button', async () => {
   render(<HomePage/>)
   
   userEvent.click(screen.getByRole('button', {
     name: /submit/i
   }))
-  await wait();
+  expect(screen.getByRole)
 })
-*/
