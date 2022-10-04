26d2e7ebb7d4168332191cc72e0b341eff7f7443
Brage Hagemann Brataas <bragehb@stud.ntnu.no>
2022-10-04 16:19:30 +0200

    Hotfix on Navigationbar.test.tsx. Added props to render function on snapshot test.

--- project2/src/tests/Navigationbar.test.tsx
+++ project2/src/tests/Navigationbar.test.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
-import Navigationbar from '../components/Navigationbar'
+import Navigationbar, { Page } from '../components/Navigationbar'
 
 const ReactTestRenderer = require('react-test-renderer');
 
+const checkAndSetPage = jest.fn();
+
 it('renders correctly', () => {
-  const tree = ReactTestRenderer.create(<Navigationbar/>).toJSON()
+  const tree = ReactTestRenderer.create(<Navigationbar page={Page.Home} checkAndSetPage={checkAndSetPage}/>).toJSON()
   expect(tree).toMatchSnapshot()
 })
